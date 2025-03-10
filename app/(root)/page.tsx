3342bf98516810654a73f48d87a0d65563c7dec4
Kundan <mokhalekundan@gmail.com>
2025-03-10 14:11:36 +0530

    current balance removed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSidebar from '@/components/RightSidebar';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 const Home = async () => {
   const loggedIn = await getLoggedInUser();
@@ -10,30 +10,25 @@ const Home = async () => {
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
-
         RECENT TRANSACTIONS
       </div>
 
-      <RightSidebar 
-        user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
-      />
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
